09cc000e6ce90ccaa38990271b94458a4ae50f40
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-01 11:36:21 -0300

    sdf

---
+++ detalles.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cityList = document.getElementById("city-list");
+    const cityContent = document.querySelector("section:last-child");
+  
+    // URL del JSON (reemplaza con la ubicación real del JSON)
+    const jsonURL = "independent.json"; // Asegúrate de que la ruta sea correcta
+  
+    // Realiza una solicitud para cargar el JSON
+    fetch(jsonURL)
+      .then((response) => response.json())
+      .then((data) => {
+         // Almacena los datos del JSON en localStorage con el nombre "datosciudad"
+         localStorage.setItem("datosciudad", JSON.stringify(data));
+        // Itera a través de todas las ciudades en el JSON
+        data.forEach((cityData) => {
+          // Obtén las traducciones en español desde "translations"
+          const translations = cityData.translations;
+          const officialNameInSpanish = translations.spa.official;
+  
+          // Genera un enlace para cada ciudad y agrégalo a la lista
+          const cityLink = document.createElement("li");
+          cityLink.innerHTML = `<a href="#${officialNameInSpanish.toLowerCase()}" onclick="showText('${officialNameInSpanish.toLowerCase()}')">${officialNameInSpanish}</a>`;
+          cityList.appendChild(cityLink);
+        });
+      })
+      .catch((error) => {
+        console.error("Error al cargar el JSON:", error);
+      });
+  });
