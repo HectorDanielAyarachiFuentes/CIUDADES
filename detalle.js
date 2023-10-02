f1f6b7db16d3edbbc8eebf60daf312db20d1cb5f
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-01 22:00:38 -0300

    456

--- detalle.js
+++ detalle.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Obtén la ID de la ciudad desde la URL
   const cityId = window.location.hash.substring(1); // Elimina el "#" del fragmento de la URL
-
   // Obtén los IDs generados aleatoriamente desde localStorage
   const generatedIds = JSON.parse(localStorage.getItem("generatedIds"));
-
   // Obtén los datos de ciudades desde localStorage
   const cityData = JSON.parse(localStorage.getItem("datosciudad"));
-
   // Busca la ciudad correspondiente por la ID generada aleatoriamente
   const selectedCity = cityData.find((city, index) => generatedIds[index] === cityId);
-
   // Verifica si se encontró la ciudad
   if (selectedCity) {
     // Muestra los detalles de la ciudad
 const cityDetails = document.getElementById("city-details");
 const population = selectedCity.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
 const cityHtml = `
 <h1>${selectedCity.translations.spa.common}</h1>
 <p>Capital: ${selectedCity.capital[0]}</p>
@@ -35,9 +30,6 @@ const cityHtml = `
 <li>Longitud: ${selectedCity.capitalInfo.latlng[1]}</li>
 </ul>
 <a id="google-maps-link" href="https://www.google.com/maps?q=${selectedCity.capitalInfo.latlng[0]},${selectedCity.capitalInfo.latlng[1]}" target="_blank">Ver en Google Maps</a>
-
-
-
 <p>Traducciones de Venezuela:</p>
 <ul>
 <li>Árabe (Oficial): ${selectedCity.translations.ara.official}</li>
--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
   const cityList = document.getElementById("city-list");
-
   // URL del JSON (reemplaza con la ubicación real del JSON)
   const jsonURL = "independent.json"; // Asegúrate de que la ruta sea correcta
-
   // Realiza una solicitud para cargar el JSON
   fetch(jsonURL)
     .then((response) => response.json())
     .then((data) => {
       // Almacena los datos del JSON en localStorage con el nombre "datosciudad"
       localStorage.setItem("datosciudad", JSON.stringify(data));
-
       // Inicializa un arreglo para almacenar las IDs generadas
       const generatedIds = [];
-
       // Itera a través de todas las ciudades en el JSON
       data.forEach((cityData, index) => {
         // Obtén las traducciones en español desde "translations"
         const translations = cityData.translations;
         const officialNameInSpanish = translations.spa.official;
-
         // Genera una ID aleatoria única
         const uniqueId = generateUniqueId();
         generatedIds.push(uniqueId);
-
         // Genera un enlace para cada ciudad con la ID aleatoria y agrégalo a la lista
         const cityLink = document.createElement("li");
         cityLink.innerHTML = `<a href="detalle.html#${uniqueId}">${officialNameInSpanish}</a>`;
         cityList.appendChild(cityLink);
-
         // Asigna un identificador único al enlace para el manejo de clics
         cityLink.dataset.cityIndex = index;
       });
-
       // Almacena los IDs generados en el localStorage
       localStorage.setItem("generatedIds", JSON.stringify(generatedIds));
     })
     .catch((error) => {
       console.error("Error al cargar el JSON:", error);
     });
-
   // Manejo de clics para toda la lista de enlaces
   cityList.addEventListener("click", function (event) {
     if (event.target.tagName === "A") {
@@ -50,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-
 // Función para generar una ID aleatoria única
 function generateUniqueId() {
   return Math.random().toString(36).substr(2, 9); // Genera una cadena de 9 caracteres alfanuméricos aleatorios
